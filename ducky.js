9d7cb64ea69d22d19e4629adee432274a19fc7b2
Avery Howard <ahoward@cedarville.edu>
2022-06-18 10:17:51 -0400

    Ducky and clouds move

--- ducky.js
+++ ducky.js
@@ -1,5 +1,25 @@
-// const canvas = document.getElementsByClassName("canvas");
-// const ctx = canvas.getContext("2d");
-// const CANVAS_WIDTH = canvas.width = 600;
-// const CANVAS_HEIGHT = canvas.height = 600;
+$(function() {
+    var x = 0;
+    setInterval(function(){
+        x+=1;
+        $(".background").css("background-position", x + "px 0px");
+    }, 50);
+})
 
+$(function() {
+    var x = 0;
+    var h = 2;
+    setInterval(function(){
+        x+=h;
+        $(".ducky").css("left", x + "px");
+
+        if (parseInt($(".ducky").css("left")) >= window.innerWidth - $(".ducky").width()) {
+            h = -h
+            $(".ducky").css("background-image", "url('Assets/DuckSprites/duckLeft.gif')");
+        }
+        else if (parseInt($(".ducky").css("left")) <= 0) {
+            h = -h
+            $(".ducky").css("background-image", "url('Assets/DuckSprites/duckRight.gif')");
+        }
+    }, 10);
+})
