53da530341c9fcc0de44ca80028abc43e3e59887
Avery Howard <ahoward@cedarville.edu>
2022-06-19 16:56:11 -0400

    Ducky go bye bye

--- ducky.js
+++ ducky.js
@@ -57,11 +57,40 @@ function returnCorndog() {
     });
 }
 
+function killDuckies() {
+    $(".ducky").each(function(i, duck) {
+        if (isSmashed($(".corndog"), $(duck))) {
+            $(duck).remove();
+        }
+    });
+}
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+function isSmashed ($div1, $div2) {
+	// Div 1 data
+	var d1_offset             = $div1.offset();
+	var d1_height             = $div1.outerHeight( true );
+	var d1_width              = $div1.outerWidth( true );
+	var d1_distance_from_top  = d1_offset.top + d1_height;
+	var d1_distance_from_left = d1_offset.left + d1_width;
+
+	// Div 2 data
+	var d2_offset             = $div2.offset();
+	var d2_height             = $div2.outerHeight( true );
+	var d2_width              = $div2.outerWidth( true );
+	var d2_distance_from_top  = d2_offset.top + d2_height;
+	var d2_distance_from_left = d2_offset.left + d2_width;
+
+	var not_colliding = ( d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left );
+
+	// Return whether it IS colliding
+	return ! not_colliding;
+};
 
 class Ducky {
     constructor(x, y, friction) {
@@ -114,6 +143,10 @@ class Ducky {
         else if (parseInt($(this.duck).css("left")) <= 0) {
             this.dir = -this.dir;
             $(this.duck).css("background-image", "url('Assets/DuckSprites/duckRight.gif')");
+        }
+
+        if (isSmashed($(".corndog"), $(this.duck))) {
+            $(this.duck).remove();
         }
     }
 }
